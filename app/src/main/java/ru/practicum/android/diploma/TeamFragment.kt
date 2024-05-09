49e7cabaa4e17f12714906723f20fd5a748cd8ff
Denis <denis.spb.2004@gmail.com>
2024-05-09 15:40:12 +0300

    GH-10 - Commit все значения убраны в dimens файл

--- app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/TeamFragment.kt
@@ -10,14 +10,13 @@ class TeamFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_team, container, false)
     }
 }
